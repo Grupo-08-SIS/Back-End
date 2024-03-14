5077f65f0b453cf1de186cf06a4d9100dec164bf
brudneyuk <brudney.ramosjr@sptech.school>
2024-03-14 20:27:03 -0300

    Add(Função de Listar usuários + Listar usuários por ID + Listar usuário por tipo de usuario + Deletar usuario)

--- rest-api/Backend/src/main/kotlin/TechForAll/Backend/controller/UsuarioController.kt
+++ rest-api/Backend/src/main/kotlin/TechForAll/Backend/controller/UsuarioController.kt
@@ -3,11 +3,7 @@ package TechForAll.Backend.controller
 import TechForAll.Backend.model.Usuario
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
@@ -36,6 +32,49 @@ class UsuarioController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario)
     }
+
+    @GetMapping()
+    fun listar(): ResponseEntity<List<Usuario>> {
+        if (listaUsuarios.isEmpty()) {
+            return ResponseEntity.status(204).build()
+        }
+        return ResponseEntity.status(200).body(listaUsuarios)
+    }
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): ResponseEntity<Usuario> {
+        val usuarioEncontrado = listaUsuarios.find { it.id == id }
+        if (usuarioEncontrado != null) {
+            return ResponseEntity.ok(usuarioEncontrado)
+        }
+        return ResponseEntity.notFound().build()
+    }
+
+    @GetMapping("/tipousuario/{tipo}")
+    fun buscarUsuariosPorTipo(@PathVariable tipo: Long): ResponseEntity<List<Usuario>> {
+        val usuariosEncontrados = listaUsuarios.filter { it.tipoUsuario.toLong() == tipo }
+        return if (usuariosEncontrados.isNotEmpty()) {
+            ResponseEntity.ok(usuariosEncontrados)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletarPorId(@PathVariable id: Long): ResponseEntity<Void> { //Void: indica q o codigo não irá retornar um corpo
+        val usuarioEncontrado = listaUsuarios.find { it.id == id }   // Ou seja, será entregue só o status.
+        if (usuarioEncontrado != null) {
+            listaUsuarios.remove(usuarioEncontrado)
+            return ResponseEntity.noContent().build()
+        }
+        return ResponseEntity.notFound().build()
+    }
+
+    fun existeUsuario(indice: Int): Boolean {
+        return indice >= 0 && indice < listaUsuarios.size
+    }
+
+
 
 
 }
